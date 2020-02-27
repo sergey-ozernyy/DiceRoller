2cfbd91badd59f69a5fd24a15069f3a31f8d16a7
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-02-27 18:56:39 +0300

    Android Kotlin Fundamentals 01.2: Basic app anatomy Coding challenge - completed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,8 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice(){
@@ -20,6 +24,19 @@ class MainActivity : AppCompatActivity() {
         //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        when (resultText.text.toString()){
+            "Hello World!" -> resultText.text = "1"
+            "6" -> return
+            else -> {
+                val x = resultText.text.toString().toInt() + 1
+                resultText.text = x.toString()
+            }
+
+        }
     }
 
 
