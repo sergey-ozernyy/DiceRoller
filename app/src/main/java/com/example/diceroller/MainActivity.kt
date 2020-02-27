830d08de0d89d3dd3925e43a833fe25d5782bc2b
sergey-ozernyy <sergey.ozernyy@mail.ru>
2020-02-27 22:49:18 +0300

    Android Kotlin Fundamentals 01.3: Image resources and compatibility Coding challenge - completed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,41 +3,58 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+
+    lateinit var diceImage1: ImageView
+    lateinit var diceImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
+
+
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
-
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener { countUp() }
-    }
-
-    private fun rollDice(){
-        val randomInt = (1..6).random()
-        //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
-    }
-
-    private fun countUp(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        when (resultText.text.toString()){
-            "Hello World!" -> resultText.text = "1"
-            "6" -> return
-            else -> {
-                val x = resultText.text.toString().toInt() + 1
-                resultText.text = x.toString()
-            }
-
+        rollButton.setOnClickListener {
+            diceImage1.setImageResource(getRandomDiceImage())
+            diceImage2.setImageResource(getRandomDiceImage())
         }
+
+
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener { reset() }
+
+
     }
+
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return drawableResource
+    }
+
+
+    private fun reset(){
+        diceImage1.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+    }
+
 
 
 }
